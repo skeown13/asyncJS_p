767730d0c229c65be04ff7162b45bc6b421e069a
skeown13 <code@keown.net>
2020-07-30 14:30:17 -0700

    added another callback in order to writeFile after getting data from previous callbacks

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
     console.log(res.body.message)
+
+    fs.writeFile('dog-img.txt', res.body.message, err => {
+      console.log("Random dog image saved to file!")
+    })
   })
 })
